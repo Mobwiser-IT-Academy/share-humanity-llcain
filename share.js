e530fff74511b961c2845f548e31b6bc02a649c5
llcain <llcain2012@gmail.com>
2020-09-07 12:53:39 -0500

    JS Object

--- share.js
+++ share.js
@@ -29,3 +29,32 @@ const navSlide = () => {
 }
 
 navSlide();
+
+
+const content = document.getElementById('content');
+const humanitarianIssue = [
+    {title: 'Shelter', summary: 'The people in Nigeria have no shelter. No place to go or call home. They live in tents, which is not shelter at all. They all of bunch up in tent villages.'},
+    {title: 'No clean drinking water', summary: 'Nigerians do not have any clean drinking water. Their water is dark brown and have bugs in it. The people get sick from drinking it.'},
+    {title: 'Hunger', summary: 'People in Nigeria are going hungry. They cannot get any food. Kids are suffering from malnourishment.'},
+    {title: 'Unable to get medical help', summary: 'Since the people in Nigeria have drinking unclean water, they get waterborne diseases. They cannot get the medical help they need to get better. Leaving alot of the kids near dealth or dying.'}
+
+    
+    
+
+
+        
+];
+
+let list = "<div>";
+
+for(i = 0; i < humanitarianIssue.length; i++) {
+    list += "<h1>" + humanitarianIssue[i].title + "</h1>";
+    list += "<p>" + humanitarianIssue[i].summary + "</p>"
+};
+list += "</div>"
+
+content.innerHTML = list;
+
+
+
+
