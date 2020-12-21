1b275fb1b695416e41071379e39dadcc19520af1
llcain-github <llcain2012@gmail.com>
2020-12-20 21:17:03 -0600

    npm install --save-dev html-loader. Also file-loader

--- webpack.common.js
+++ webpack.common.js
@@ -13,8 +13,23 @@ module.exports = {
                 use: [
                     "style-loader", // Inject styles into DOM
                      "css-loader" // Turns css into common JS
-                    ]                
+               ]                
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
             }
+
         ]
     }
 };
